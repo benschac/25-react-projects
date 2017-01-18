69c38fc9f0e68d82aadedb2bb1f2b17b620a14bd
Bejamin Schachter <benschac@gmail.com>
2017-01-18 12:56:32 -0500

    renders players to the DOM

--- nhl-player-search/src/App.js
+++ nhl-player-search/src/App.js
@@ -4,11 +4,30 @@ import List from './List';
 import './App.css';
 
 class App extends Component {
+  getData() {
+    if(self.fetch) {
+        // run my fetch request here
+        let url = 'https://rawgit.com/benschac/ea8eacba1121711e2e542732e6d3e12d/raw/3eff20b38cd2a8f7c36c5f3e7f196442338a2eab/active-nhl-players.json';
+        fetch(url).then((response) => {
+          if(response.status !== 200) {
+            throw Error('Something went wrong' + response.status);
+          }
+          response.json().then((data) => {
+            console.log('block fired');
+            return Promise.resolve(data);
+          });
+        })
+        .catch((err) => {
+          console.log('Error with the Fetch API');
+        });
+    }
+  }
+
   render() {
     return (
       <div>
       <Filter />
-      <List />
+      <List players={this.getData()}/>
       </div>
     );
   }
--- nhl-player-search/src/Filter.js
+++ nhl-player-search/src/Filter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class Filter extends Component {
 	constructor(props) {
 		super(props);
