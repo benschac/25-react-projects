3daa8160f818c0c86a2a218f809edf572b4abb5e
Bejamin Schachter <benschac@gmail.com>
2017-02-21 11:31:13 -0500

    more handling and lifting up state, creates user checks

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -7,7 +7,8 @@ export default class Board extends React.Component {
 
     this.state = {
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
   }
   renderSquare(i) {
@@ -18,13 +19,27 @@ export default class Board extends React.Component {
   handleClick(i) {
     // clone state;
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
+    // game was won or it's been clicked before.
+    if(calculateWinner(this.state.squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
     // es6 short cut.
-    this.setState({squares});
+    this.setState({squares,
+                  xIsNext: !this.state.xIsNext});
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if(winner) {
+      status = `Player ${winner} has won the game`;
+    } else {
+      status = `Player ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -46,4 +61,25 @@ export default class Board extends React.Component {
       </div>
     );
   }
+}
+
+// Stateless functional Component
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
--- tic-tac-toe/src/Game.js
+++ tic-tac-toe/src/Game.js
@@ -3,6 +3,7 @@ import './App.css';
 import Board from './Board';
 
 export default class Game extends React.Component {
+
   render() {
     return (
       <div className="game">
@@ -16,27 +17,4 @@ export default class Game extends React.Component {
       </div>
     );
   }
-}
-
-// ========================================
-
-// Stateless functional Component
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
