718e225ba119532448290876aa942761d9ce2a8f
Bejamin Schachter <benschac@gmail.com>
2017-02-23 09:41:40 -0500

    finish react tic-tac-toe tutorial

--- tic-tac-toe/src/Board.js
+++ tic-tac-toe/src/Board.js
@@ -2,47 +2,20 @@ import React from 'react';
 import Square from './Square';
 
 export default class Board extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
-      squares: Array(9).fill(null),
-      xIsNext: true
-    }
-  }
   renderSquare(i) {
     {/* Takes in param, sets value */}
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+    const squares = this.props.squares;
+    return <Square value={squares[i]} onClick={() => this.props.onClick(i)}/>;
   }
   // passed into render square which is passed to component.
-  handleClick(i) {
-    // clone state;
-    const squares = this.state.squares.slice();
-    // game was won or it's been clicked before.
-    if(calculateWinner(this.state.squares) || squares[i]) {
-      return;
-    }
 
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-
-    // es6 short cut.
-    this.setState({squares,
-                  xIsNext: !this.state.xIsNext});
-  }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner) {
-      status = `Player ${winner} has won the game`;
-    } else {
-      status = `Player ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
+
 
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -61,25 +34,4 @@ export default class Board extends React.Component {
       </div>
     );
   }
-}
-
-// Stateless functional Component
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
--- tic-tac-toe/src/Game.js
+++ tic-tac-toe/src/Game.js
@@ -2,19 +2,105 @@ import React, { Component } from 'react';
 import './App.css';
 import Board from './Board';
 
+
 export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+      stepNumber: 0
+    }
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ? false : true,
+    })
+  }
+
+  handleClick(i) {
+    var history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    // game was won or it's been clicked before.
+    if(calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+    // es6 short cut.
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
 
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+    let status;
+
+    if(winner) {
+      status = `Player ${winner} has won the game`;
+    } else {
+      status = `Player ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li key={move}>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
+
     return (
+
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={current.squares}
+                onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+        <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
+}
+
+
+// Stateless functional Component
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
