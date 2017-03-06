5926a27d07da0bbe36bda77518e91e2f6eea3108
Bejamin Schachter <benschac@gmail.com>
2017-03-06 12:18:49 -0500

    sets up inital fetch requests

--- leaderboard/src/components/App.js
+++ leaderboard/src/components/App.js
@@ -9,6 +9,25 @@ class App extends Component {
    * Top 100 users 30 days endpoint: https://fcctop100.herokuapp.com/api/fccusers/top/recent.
    * Top 100 users all time: https://fcctop100.herokuapp.com/api/fccusers/top/alltime.
    */
+  
+  componentWillMount() {
+    let headers = new Headers();
+    let url30 = `https://fcctop100.herokuapp.com/api/fccusers/top/recent`;
+    let options = {
+      method: 'GET',
+      headers,
+      mode: 'cors',
+      cache: 'default'
+    }
+    
+    fetch(url30, options)
+    .then(val => {
+      return val.json();
+    })
+    .then(val => console.log(val))
+    .catch(err => {throw err});
+  }
+  
 
   render() {
     return (
