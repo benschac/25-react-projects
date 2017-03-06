601549d50e7c0891567f733e982a4207fa6f36b7
Bejamin Schachter <benschac@gmail.com>
2017-03-06 13:55:04 -0500

    Creates a promise chain and sets state

--- leaderboard/src/components/App.js
+++ leaderboard/src/components/App.js
@@ -9,29 +9,60 @@ class App extends Component {
    * Top 100 users 30 days endpoint: https://fcctop100.herokuapp.com/api/fccusers/top/recent.
    * Top 100 users all time: https://fcctop100.herokuapp.com/api/fccusers/top/alltime.
    */
+  constructor(props) {
+    super(props);
+    this.state = {
+      allTimeUsers: [],
+      recentUsers: []
+    }
+  }
   
   componentWillMount() {
     let headers = new Headers();
-    let url30 = `https://fcctop100.herokuapp.com/api/fccusers/top/recent`;
+    let urlAlltime = `https://fcctop100.herokuapp.com/api/fccusers/top/alltime`;
+    let urlrecent = `https://fcctop100.herokuapp.com/api/fccusers/top/recent`;
+    let allTimeData = getData(urlAlltime);
+    let recentData = getData(urlrecent);
     let options = {
       method: 'GET',
       headers,
       mode: 'cors',
       cache: 'default'
     }
+
+    function getData(url) {
+      return fetch(url, options)
+      .then(val => {
+        return val.json();
+      })
+      .then(val => {
+         return val;
+      })
+      .catch(err => {throw err});
+    }
     
-    fetch(url30, options)
-    .then(val => {
-      return val.json();
-    })
-    .then(val => console.log(val))
-    .catch(err => {throw err});
+    Promise.all([allTimeData, recentData])
+      .then(res => {
+        console.log('alltime', res[0], 'recent', res[1]);
+        this.setState({
+          allTimeUsers: res[0],
+          recentUsers: res[1]
+        })
+      })
+      .catch(err => {throw err});
   }
+    
   
 
   render() {
+  const listUsersAlltime = this.state.allTimeUsers.map((user) =>
+    <li>{user.username}</li>
+    );
     return (
       <div className="leaderboard">
+        <ul>
+          <li>{listUsersAlltime}</li>
+        </ul>
         <Header />
         <UsersTable />
         <Footer />
