5c52001a6b18c72040b7b53f633d6f2b9ee30e90
Bejamin Schachter <benschac@gmail.com>
2017-03-06 11:44:37 -0500

    remove boilerplate, create dumb layout

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  /**
+   * Top 100 users 30 days endpoint: https://fcctop100.herokuapp.com/api/fccusers/top/recent.
+   * Top 100 users all time: https://fcctop100.herokuapp.com/api/fccusers/top/alltime.
+   */
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="leaderboard">
+        <div className="leaderboard-header">
+          <h2>Leaderboard</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+        <tr>
+          <th>#</th>
+          <th>Camper Name</th>
+          <th>Points in past 30 days</th>
+          <th>All time points</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>benschac</td>
+          <td>400</td>
+          <td>1000</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>benschac</td>
+          <td>400</td>
+          <td>1000</td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>benschac</td>
+          <td>400</td>
+          <td>1000</td>
+        </tr>
+        </table>
+        <footer>
+          <h3>By Benjamin Schachter</h3>
+        </footer>
       </div>
     );
   }
