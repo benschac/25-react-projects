74893cf87b9046eb6ee2397e24ddb8e2d69b77c9
Bejamin Schachter <benschac@gmail.com>
2017-03-15 10:48:20 -0400

    updates app component

--- game-of-life/src/components/App.js
+++ game-of-life/src/components/App.js
@@ -4,14 +4,27 @@ import BoardContainer from './BoardContainer';
 import Controls from './Controls';
 import './App.css';
 
+function Pixel() {
+  return (
+    <div className></div>
+  )
+}
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       size: 20,
-      
+      isEvolving: false,
+      generation: 0,
+      speed: 1000,
+      pixels: [
+        {}
+      ]
     }
   }
+
+
   render() {
     return (
       <div className="App">
