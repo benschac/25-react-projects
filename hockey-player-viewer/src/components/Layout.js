2be3df303ab2f0790784ee3812e9c2fe639e278c
Bejamin Schachter <benschac@gmail.com>
2017-03-17 16:25:54 -0400

    adds layout component

---
+++ hockey-player-viewer/src/components/Layout.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Layout extends Component {
+    
+    render() {
+        const { children } = this.props;
+        return (
+            <div className="app-container">
+                <header>
+                    <Link to="/">
+                        <img className="logo" src="/img/logo-judo-heros.png"/>
+                    </Link>
+                </header>
+                <div className="app-content">
+                    { children }
+                </div>
+                <footer>
+                    This is was an application built with universal javascript setup using React + Express.
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Layout;
