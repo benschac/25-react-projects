da5c97b139fe262ce027ad6a893f828601ba622f
Bejamin Schachter <benschac@gmail.com>
2017-03-17 16:51:09 -0400

    adds PlayerIndex + PageNotFound

---
+++ hockey-player-viewer/src/components/AthletePage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import NotFoundPage from './NotFoundPage';
+import AthletesMenu from './AthletesMenu';
+import Medal from './Medal';
+import Flag from './Flag';
+import athletes from '../data/athletes';
+
+
+
+class AthletePage extends Component {
+    
+    render() {
+        const { id } = this.props.params;
+        const athlete = athletes.filter(athlete => athlete.id === id)[0];
+        if(!athlete) {
+            return <NotFoundPage />;
+        }
+
+        const headerStyle = { backgroundImage: `url(/img/${athlete.cover})`};
+        return (
+            <div className="athlete-full">
+                <AthletesMenu athletes={athletes}/>
+                    <div className="athlete">
+                        <header style={headerStyle} />
+                        <div className="picture-container">
+                            <img src={`/img/${athlete.image}`}/>
+                            <h2 className="name">{athlete.name}</h2>
+                        </div>
+                    </div>
+                <section className="description">
+                    Olympic medalist from <strong><Flag code={athlete.country} showName="true"/></strong>,
+                    born in {athlete.birth} (Find out more on <a href={athlete.link} target="_blank">Wikipedia</a>).
+                </section>
+                <section className="medals">
+                    <p>Winner of <strong>{athlete.medals.length}</strong> medals:</p>
+                    <ul>{
+                    athlete.medals.map((medal, i) => <Medal key={i} {...medal}/>)
+                    }</ul>
+                </section>
+                <div className="navigateBack">
+                <Link to="/">« Back to the index</Link>
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+export default AthletePage;
---
+++ hockey-player-viewer/src/components/IndexPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import AthletePreview from './AthletePreview';
+import athletes from '../data/athletes';
+
+
+class IndexPage extends Component {
+    
+    render() {
+        return (
+            <div className="home">
+                <div className="athletes-selector">
+                    { athletes.map(athleteData => <AthletePreview
+                                                    key={athleteData.id}
+                                                    {...athleteData} />)}
+                </div>
+            </div>        
+        );
+    }
+}
+
+export default IndexPage;
