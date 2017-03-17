d919b4fa87868d6604c123953bb50ca035b0b826
Bejamin Schachter <benschac@gmail.com>
2017-03-17 15:38:50 -0400

    starts project

---
+++ hockey-player-viewer/src/components/Flag.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+// Will be updated with NHL API.
+const data = {
+  'cu': {
+    'name': 'Cuba',
+    'icon': 'flag-cu.png',
+  },
+  'fr': {
+    'name': 'France',
+    'icon': 'flag-fr.png',
+  },
+  'jp': {
+    'name': 'Japan',
+    'icon': 'flag-jp.png',
+  },
+  'nl': {
+    'name': 'Netherlands',
+    'icon': 'flag-nl.png',
+  },
+  'uz': {
+    'name': 'Uzbekistan',
+    'icon': 'flag-uz.png',
+  }
+};
+
+class Flag extends Component {
+    
+    render() {
+    const { code, showName } = this.props;
+    const name = data[code].name;
+    const icon = data[code].icon;
+
+        return (
+            <span className="flag">
+                <img className="icon" title={name} src={`img/${icon}`}/>
+                /**
+                 * If name is provided show it.
+                 */
+                { showName && <span className="name"> {name} </span> }
+            </span>        
+        );
+    }
+}
+
+export default Flag;
