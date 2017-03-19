cc53754fd37bb0fa4d0cfabeb38c2519b9740e62
Bejamin Schachter <benschac@gmail.com>
2017-03-19 16:59:00 -0400

    adds reducer tests

--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import expect from 'expect';
+import deepFreeze from 'deepfreeze';
+import { todos } from './index.js'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+describe('todos reducer', () => {
+  it('should handle inital state', () => {
+    expect(
+      todos(undefined, {})
+    ).toEqual([])
+  })
+
+  it('should handle ADD_TODO', () => {
+    const stateBefore = [];
+    const action = {
+      type: "ADD_TODO",
+      id: 0,
+      text: 'Learn Redux',
+    }
+    const stateAfter = [{
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    }];
+    
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+      todos(stateBefore, action)
+    ).toEqual(stateAfter);
+
+  });
 });
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,58 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import expect from 'expect';
-import deepFreeze from 'deepfreeze';
 import './index.css';
 
-const todos = (state = [], action ) => {
+export const todos = (state = [], action ) => {
   switch(action.type) {
     case 'ADD_TODO':
       return [
         ...state,
         {
-          type: action.type,
           id: action.id,
-          text: action.text
+          text: action.text,
+          completed: false
         },
       ]
     default:
       return state;
   }
 }
-
-const testAddToDo = () => {
-  const stateBefore = [];
-  const stateAfter = [{
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  }];
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
-  }
-
-  deepFreeze(stateBefore);
-  deepFreeze(action);
-  expect(
-    todos(stateBefore, action)
-  ).toBe(stateAfter);
-}
-
-
-class App extends Component {
-  
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello From App.js</h1>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
