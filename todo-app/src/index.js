ac217d2b4908655b928545a224f4c960c512f25e
Bejamin Schachter <benschac@gmail.com>
2017-03-19 16:30:11 -0400

    creates todo reducer and tests.

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import expect from 'expect';
+import deepFreeze from 'deepfreeze';
 import './index.css';
+
+const todos = (state = [], action ) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          type: action.type,
+          id: action.id,
+          text: action.text
+        },
+      ]
+    default:
+      return state;
+  }
+}
+
+const testAddToDo = () => {
+  const stateBefore = [];
+  const stateAfter = [{
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  }];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux'
+  }
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+  expect(
+    todos(stateBefore, action)
+  ).toBe(stateAfter);
+}
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello From App.js</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <App />,
