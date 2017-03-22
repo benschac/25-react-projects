a9413921e77a2c010b2e6205528e399d222b3876
Bejamin Schachter <benschac@gmail.com>
2017-03-22 10:26:44 -0400

    adds reducers

--- reddit-reader/src/actions.js
+++ reddit-reader/src/actions.js
@@ -18,5 +18,25 @@ export function invalidateSubreddit(subreddit) {
 }
 
 
+// Asynchronous Actions
+export const REQUEST_POSTS = 'REQUEST_POSTS';
+export const RECEIVE_POSTS = 'RECEIVE_POSTS';
+
+function requestPosts(subreddit) {
+    return {
+        type: REQUEST_POSTS,
+        subreddit
+    }
+}
+
+function receivePosts(subreddit, json) {
+    return {
+        type: RECEIVE_POSTS,
+        subreddit,
+        posts: json.data.children.map(child => child.data),
+        receivedAt: Date.now()
+    }
+}
+
 
 
