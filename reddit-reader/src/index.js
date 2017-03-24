98e78363ed8cea57435e85683c35bd9cbfcc4059
Bejamin Schachter <benschac@gmail.com>
2017-03-24 11:41:27 -0400

    adds actions, reducers, imports to index.js

--- reddit-reader/src/index.js
+++ reddit-reader/src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux'
+import { selectSubreddit, fetchPosts } from './actions'
+import rootReducer from './reducers'
 import './index.css';
+
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
+store.dispatch(selectSubreddit('reactjs'));
+store.dispatch(fetchPosts('reactjs')).then(() => {
+  console.log(store.getState());
+})
 
 ReactDOM.render(
   <App />,
