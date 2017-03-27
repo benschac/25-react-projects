ef8980771db8d28641c23bd2243278addcbef8a2
Bejamin Schachter <benschac@gmail.com>
2017-03-27 19:10:15 -0400

    adds bootstrap styling

--- recipe-box/src/App.jsx
+++ recipe-box/src/App.jsx
@@ -24,22 +24,26 @@ class App extends Component {
           recipe1: {
             edit: false,
             title: 'Pizza',
-            items: ["Cheese", "Tomato", "Dough"]
+            items: ["Cheese", "Tomato", "Dough"],
+            hidden: true
           },
           recipe2: {
             edit: false,
             title: 'PBJ',
-            items: ["Bread", "Peanut Butter", "Jelly"]
+            items: ["Bread", "Peanut Butter", "Jelly"],
+            hidden: true
           },
           recipe3: {
             edit: false,
             title: 'Ramen',
-            items: ["Egg", "Veggies", "Noodles", "Broth", "Mushrooms"]
+            items: ["Egg", "Veggies", "Noodles", "Broth", "Mushrooms"],
+            hidden: true
           },
           recipe4: {
             edit: false,
             title: 'Burger',
-            items: ["Buns", "Beef Patty", "Tomato", "Lettuce", "Onions"]
+            items: ["Buns", "Beef Patty", "Tomato", "Lettuce", "Onions"],
+            hidden: true
           }
         }
     }
@@ -95,6 +99,13 @@ class App extends Component {
     this.setState({recipes});
   }
 
+  toggleItems = (key) => {
+    let recipes = {...this.state.recipes}
+
+    recipes[key].hidden = !recipes[key].hidden
+    this.setState({recipes})
+  }
+
   componentWillUpdate(nextProps, nextState) {
     if(storageAvailable('localStorage')) {
       const ref = localStorage.setItem('recipes', JSON.stringify(nextState.recipes))
@@ -123,37 +134,42 @@ class App extends Component {
  
     return (
       <div className="app__container">
-        <h1>Recipe Box</h1>
+        <div className="page-header">
+          <h1>Recipe Box</h1>
+        </div>
       {
         Object.keys(recipes).map((recipe, index) => {
           return (
-            <div>
-              <h2 onClick={() => console.log('clicked')} key={recipe}>
+            <div className="well">
+              <h2 onClick={() => this.toggleItems(recipe)} key={recipe}>
                   {recipes[recipe].title}</h2>
-              <ul>
-                {
-                  
-                  recipes[recipe].items.map((item, index) => {
-                    return <li key={index}>{item}</li>
-                  })
-                }
-              </ul> 
-              <button onClick={() => this.deleteRecipe(recipe)} >Delete</button>
-              <button onClick={() => this.editToggle(recipe)}>Edit</button>
+                  {
+                    recipes[recipe].hidden ? 
+                    <ul>
+                      {
+                        
+                        recipes[recipe].items.map((item, index) => {
+                          return <li className="list-group-item" key={index}>{item}</li>
+                        })
+                      }
+                    </ul> : ""
+                  }
+              <button className="btn btn-danger" onClick={() => this.deleteRecipe(recipe)} ><span className="glyphicon glyphicon-remove" aria-hidden="true"></span> Delete</button>
+              <button className="btn btn-default" onClick={() => this.editToggle(recipe)}>Edit <span className="glyphicon glyphicon-triangle-bottom" aria-hidden="true"></span></button>
               {
                 recipes[recipe].edit ?
-              <form ref={(input) => this.editForm = input } onSubmit={(e) => this.editRecipe(e, recipe)}>
+              <form className="input-group" ref={(input) => this.editForm = input } onSubmit={(e) => this.editRecipe(e, recipe)}>
                 <div>
                   <label>Title</label>
                   <br />
-                  <input defaultValue={recipes[recipe].title} ref={(input) => this.editTitle = input }/>
+                  <input className="form-control" defaultValue={recipes[recipe].title} ref={(input) => this.editTitle = input }/>
                 </div>
                 <div>
                   <label>Ingredients</label>
                   <br />
-                <textarea defaultValue={recipes[recipe].items.join(',')} ref={(input) => this.editTextarea = input}></textarea>
+                <textarea className="form-control" defaultValue={recipes[recipe].items.join(',')} ref={(input) => this.editTextarea = input}></textarea>
                 </div>
-                <button type="submit" value="Submit">Submit</button>
+                <button className="btn btn-primary" type="submit" value="Submit"> Submit <span className="glyphicon glyphicon-ok" aria-hidden="true"></span></button>
             </form> :
               null
 
@@ -165,10 +181,10 @@ class App extends Component {
       }
       <form ref={(input) => this.form = input }onSubmit={this.createRecipe}>
         <label>Title</label>
-        <input ref={(input) => this.title = input }/>
+        <input className="form-control" ref={(input) => this.title = input }/>
         <label>Ingredients</label>
-        <textarea ref={(input) => this.textarea = input}></textarea>
-        <button type="submit" value="Submit" >Submit</button>
+        <textarea className="form-control" ref={(input) => this.textarea = input}></textarea>
+        <button className="btn btn-primary" type="submit" value="Submit" >Submit</button>
       </form>
       </div>
       
